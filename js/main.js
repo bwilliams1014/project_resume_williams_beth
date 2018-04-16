a78fb2b4a7664ca6df0df81e21c80abf4c9286e7
Beth Williams <bwilliams1014@gmail.com>
2018-04-16 18:40:43 -0400

    Clean up and add comments to CSS and JS files

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 $(document).ready(function() {
 
+// fancybox jquery plugin
 $('[data-fancybox="imgList"]').fancybox();
 
+// change cursor when mousing over images in gallery
 $('figure').mouseover(function() {
 	$(this).css({'cursor':'pointer'});
 });
 
+// change nav tabs when moused over
 $('.smoothscroll').mouseover(function() {
 	$(this).css({'background':'linear-gradient(#dd390d, #912608)'});
 });
 
+// change nav tabs back when moused away
 $('.smoothscroll').mouseleave(function() {
 	$(this).css({'background':'#303030'});
 });
 
-$('.smoothscroll').smoothScroll({'offset':'40'});
+// set smoothscroll offset so that it appears correctly
+$('.smoothscroll').smoothScroll({'offset':'55'});
 
+// sticky nav jquery plugin
 $('nav').sticky({topSpacing:0});
 
+// add animation to header
 $('header').mouseover(function() {
 	$('#myLogo').animate({'left': '40%'},'slow');
 });
